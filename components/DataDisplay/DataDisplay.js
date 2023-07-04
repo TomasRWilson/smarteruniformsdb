af585fa0d2c01e89a7cc5632ff1a48037c1f7210
TomasRWilson <tomas.robert.wilson@gmail.com>
2023-07-04 14:58:04 +0100

    All input working

--- components/DataDisplay/DataDisplay.js
+++ components/DataDisplay/DataDisplay.js
@@ -6,35 +6,18 @@ import Table from "../Table/Table";
 
 const supabase = createClientComponentClient();
 
-export default function DataDisplay(props){
+export default async function DataDisplay(props){
 
-  const [schools, setSchools] = useState(props.data)
-
-  useEffect(() => {
-    setSchools(props.data)
-  }, [props.data])
-
-  useEffect(() => {
-    const channel = supabase
-      .channel('*')
-      .on('supabase_realtime', { event: 'INSERT', schema: 'public', table: props.table }, (payload) =>
-        setSchools((schools) => [...schools, payload.new])
-      )
-      .subscribe()
-
-    return () => {
-      supabase.removeChannel(channel)
-    }
-  }, [props.data])
+  const [data, setData] = useState(props.data)
 
     const getHeadings = () => {
-        return Object.keys(schools[0]);
+        return Object.keys(data[0]);
       }
     
       try{
         return(
             <>
-                <Table theadData={getHeadings()} tbodyData={schools}/>
+                <Table theadData={getHeadings()} tbodyData={data}/>
             </>
         )
       }catch{
